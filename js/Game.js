12a99ecab2973e0f43a601de8e7bdc58dc93ae2e
Snir Holland <sniro84@hotmail.com>
2019-07-26 15:31:06 +0300

    added phrase to screen

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,43 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game
+{
+    constructor()
+    {
+       this.missed = 0;
+       this.phrases = this.createPhrases();
+       this.activePhrase = null;
+    }
+
+    /**
+     * Creates phrases for use in game
+     * @return {array} An array of phrases that could be used in the game
+     */
+    createPhrases()
+    {
+        const phrases = [
+            new Phrase("Curiosity Killed The Cat"),
+            new Phrase("Beauty Is In The Eye Of The Beholder"),
+            new Phrase("You Cant Have Your Cake And Eat It Too"),
+            new Phrase("Honesty is the best policy"),
+            new Phrase("Laughter is the best medicine"),
+            new Phrase("The grass is always greener on the other side"),
+            new Phrase("Two wrongs dont make a right")
+        ]
+        
+        return phrases;
+    }
+
+    /**
+     * Selects random phrase from phrases property
+     * @return {Object} Phrase object chosen to be used
+     */
+    getRandomPhrase() 
+    {
+        const randomIndex = Math.floor((Math.random() * this.phrases.length));
+        return this.phrases[randomIndex];
+    }
+
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,36 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase
+{
+    constructor(phrase)
+    {
+      this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Display phrase on game board
+     */
+    addPhraseToDisplay() 
+    {
+        const phraseDiv = document.querySelector('#phrase');
+        const currUl = document.querySelector('#phrase > ul');
+        const newPharseUl = document.createElement('ul');
+        phraseDiv.replaceChild(newPharseUl,currUl);
+        
+        for(let i=0; i<this.phrase.length; i++)
+        {
+            const currLi = document.createElement('li');
+            const phraseChar = this.phrase.charAt(i);
+
+            if (phraseChar === ' ')
+                currLi.setAttribute('class',"space");
+            else
+                currLi.setAttribute('class',`hide letter ${phraseChar}`);
+
+            currLi.textContent = `${phraseChar}`;
+            newPharseUl.appendChild(currLi);
+        }  
+    }
+}
